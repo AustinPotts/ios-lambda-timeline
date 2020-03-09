6b5c430b7068ad33c85b6161b1de4f40b10782ea
AustinPotts <pottsevolvements@gmail.com>
2020-03-09 16:47:57 -0400

    Configured storyboard + outlets + actions

--- ImageFilters/ImageFilters/ViewController.swift
+++ ImageFilters/ImageFilters/ViewController.swift
@@ -15,6 +15,40 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+  //MARK: - Outlets
+    
+    @IBOutlet weak var brightnessSlider: UISlider!
+    @IBOutlet weak var contrastSlider: UISlider!
+    @IBOutlet weak var saturationSlider: UISlider!
+    @IBOutlet weak var imageViewer: UIImageView!
+    
+    
+    
+    @IBAction func selectImageTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    
+    //MARK: - Slider Events
+    @IBAction func brightnessChanged(_ sender: UISlider) {
 
+   
+        
+    }
+    
+    @IBAction func contrastChanged(_ sender: Any) {
+
+
+    }
+    
+    @IBAction func saturationChanged(_ sender: Any) {
+        
+
+    }
+    
+    
 }
 
