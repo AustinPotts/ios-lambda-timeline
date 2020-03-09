c87c9a99529658d43a5271fa724e2fd77cab7149
AustinPotts <pottsevolvements@gmail.com>
2020-03-09 16:31:03 -0400

    Configured ImageFilters Project

---
+++ ImageFilters/ImageFilters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageFilters
+//
+//  Created by Austin Potts on 3/9/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
